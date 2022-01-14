c278766f92983ce8d413b84c19ff88a04a78159a
haider000 <mohd.b.haider000786@gmail.com>
2022-01-14 05:53:35 +0530

    ✨ added support for PR rules

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,66 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const github_token = core.getInput('GITHUB_TOKEN')
+    const ocktoKit = github.getOctokit(github_token)
+    const context = github.context
+    const sha = context.sha
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const result =
+      await ocktoKit.rest.repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: sha
+      })
 
-    core.setOutput('time', new Date().toTimeString())
+    const pullRequests = result.data.filter(
+      pullRequest => pullRequest.state === 'open'
+    )
+
+    const pr = pullRequests.length > 0 && pullRequests[0]
+
+    if (!pr) return
+
+    const prFromBranch = pr.base.ref
+    const prToBranch = pr.head.ref
+
+    core.debug(JSON.stringify({prFromBranch, prToBranch}))
+
+    const rulesInput = core.getInput('rules')
+    const rules = JSON.parse(rulesInput) as string[]
+
+    // convert to an array
+    const conditions = rules.map(str => {
+      const arr = str.split(' ') // [ 'allow', 'if', 'from', 'dev', 'to', 'master' ]
+      const allow = arr[0] === 'allow'
+      const fromBranch = arr[3]
+      const toBranch = arr[5]
+      return {allow, fromBranch, toBranch, rule: str}
+    })
+
+    let matchedRule = ''
+    // it will pass or fail based on the first condition that matches
+    for (const condition of conditions) {
+      const fromMatch = new RegExp(condition.fromBranch).test(prFromBranch)
+      const toMatch = new RegExp(condition.toBranch).test(prToBranch)
+      if (fromMatch && toMatch) {
+        if (condition.allow) {
+          // if a rule matches skip and pass the test
+          core.debug(`Passed: Rule matched "${condition.rule}"`)
+          matchedRule = condition.rule
+          break
+        } else {
+          // otherwise fail the action
+          core.setFailed(`Failed: Rule not matched "${condition.rule}"`)
+        }
+      }
+    }
+
+    if (!matchedRule) {
+      core.debug('Passed: No rules matched')
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
