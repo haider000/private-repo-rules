914d2442359239555ba0d0f7e34d82e7bf630295
haider000 <mohd.b.haider000786@gmail.com>
2022-01-16 06:47:05 +0530

    🐛 final fix

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,17 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const github_token = core.getInput('GITHUB_TOKEN')
-    const ocktoKit = github.getOctokit(github_token)
     const context = github.context
-    const sha = context.sha
-    console.log(context.payload.pull_request)
-    console.log(context.payload.pull_request?.base)
-    console.log(context.payload.pull_request?.head)
-    console.log(context.repo.owner, context.repo.repo, sha)
-    const result =
-      await ocktoKit.rest.repos.listPullRequestsAssociatedWithCommit({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        commit_sha: sha
-      })
 
-    console.log(result.data)
-    const pullRequests = result.data.filter(
-      pullRequest => pullRequest.state === 'open'
-    )
+    if (!context.payload.pull_request) {
+      core.setFailed('No PR Found, only run when pull request happens')
+      return
+    }
 
-    console.log(pullRequests)
+    const prFromBranch = context.payload.pull_request.base.ref as string
+    const prToBranch = context.payload.pull_request.head.ref as string
 
-    const pr = pullRequests.length > 0 && pullRequests[0]
-
-    if (!pr) return
-
-    const prFromBranch = pr.base.ref
-    const prToBranch = pr.head.ref
-
-    console.log(JSON.stringify({prFromBranch, prToBranch}))
+    console.log({prFromBranch, prToBranch})
 
     const rulesInput = core.getInput('rules')
     const rules = JSON.parse(rulesInput) as string[]
@@ -46,7 +27,7 @@ async function run(): Promise<void> {
       return {allow, fromBranch, toBranch, rule: str}
     })
 
-    console.log(JSON.stringify(conditions))
+    console.log('conditions', conditions)
 
     let matchedRule = ''
     // it will pass or fail based on the first condition that matches
@@ -56,7 +37,7 @@ async function run(): Promise<void> {
       if (fromMatch && toMatch) {
         if (condition.allow) {
           // if a rule matches skip and pass the test
-          core.debug(`Passed: Rule matched "${condition.rule}"`)
+          core.info(`Passed: Rule matched "${condition.rule}"`)
           matchedRule = condition.rule
           break
         } else {
@@ -67,7 +48,7 @@ async function run(): Promise<void> {
     }
 
     if (!matchedRule) {
-      core.debug('Passed: No rules matched')
+      core.info('Passed: No rules matched')
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
